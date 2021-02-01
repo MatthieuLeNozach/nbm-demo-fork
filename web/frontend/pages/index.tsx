6bc2cec27d96ffd8f25b6084071b5a21b3519798
Gaëtan <gaetan_duhamel@natural-solutions.eu>
2021-02-01 10:58:43 +0100

    core(web) : NextJS setup wip

--- web/frontend/pages/index.tsx
+++ web/frontend/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTheme } from "@material-ui/core/styles";
 import { Button } from "../components/button";
-import { Example } from "./example/example";
 
 const Home = () => {
   const theme = useTheme();
@@ -10,7 +9,6 @@ const Home = () => {
     <>
       <div style={{ color: theme.palette.primary.main }}>{hello}</div>
       <Button name="Click Me" color="primary" />
-      <Example />
     </>
   );
 };
