531aab40f112dbc066b6b01805be09c08dbfd8aa
Javi Blanco <jblancogl@gmail.com>
2021-03-31 14:55:38 +0000

    feat(pytest): add --nextcloud_active option in pytest to skip nextcloud tests on an unconfigured environment

--- web/scripts/test.sh
+++ web/scripts/test.sh
@@ -11,8 +11,9 @@ docker-compose -p tests \
 -f docker-compose.yml \
 config > docker-stack.yml
 
-docker-compose -p tests -f docker-stack.yml build
+docker-compose -p tests -f docker-stack.yml build --build-arg app_user=${USER}
 docker-compose -p tests -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 docker-compose -p tests -f docker-stack.yml up -d
+sleep 60
 docker-compose -p tests -f docker-stack.yml exec -T backend bash /app/tests-start.sh "$@"
 docker-compose -p tests -f docker-stack.yml down -v --remove-orphans
