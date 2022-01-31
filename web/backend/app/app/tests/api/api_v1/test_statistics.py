d0553f36d89994aff551e14be578bc9d9850379c
Javi Blanco <jblancogl@gmail.com>
2022-01-31 17:06:09 +0000

    Statistics sort by total

--- web/backend/app/app/tests/api/api_v1/test_statistics.py
+++ web/backend/app/app/tests/api/api_v1/test_statistics.py
@@ -14,9 +14,15 @@ def test_get_annotations_species(client: TestClient, db: Session) -> None:
     create_random_medialabel(db, label_id=standardlabel.id)
 
     response = client.get(f"{settings.API_V1_STR}/statistics/species_annotations")
+    statistics = response.json()
 
-    for item in response.json():
+    for item in statistics:
         if item['id'] == species.id:
             assert item['total'] == 2
 
     assert response.status_code == 200
+    
+    # check it is sorted by total
+    orderer = sorted(statistics, key=lambda d:d["total"], reverse=True)
+    assert orderer == statistics
+
